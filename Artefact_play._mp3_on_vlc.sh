cc5a5a784caf8f7a232880c2259eef2877d87f1f
theorbitofmercury <75587982+theorbitofmercury@users.noreply.github.com>
2026-04-12 17:34:54 +0100

    Update Artefact_play._mp3_on_vlc.sh

--- Artefact_play._mp3_on_vlc.sh
+++ Artefact_play._mp3_on_vlc.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
 
-vlc /home/mike/Music/Bobby Darin/Bobby Darin - Dream Lover.mp3
+ffplay '/home/mike/Music/Bobby Darin/Bobby Darin - Dream Lover.mp3'
+
 echo Music complete
 exit 0 # EXIT_SUCCESS
